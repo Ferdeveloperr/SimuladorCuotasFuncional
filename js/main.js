16313b3f2d7a7407fb981c6b3f2bff62f938f4e4
Fernando Gomez <fernandojgom1395@gmail.com>
2023-09-09 19:22:57 -0300

    se implementan condicionales y ciclos para validar que los datos sean correctos para la funcion definida

--- js/main.js
+++ js/main.js
@@ -1,16 +1,36 @@
+// SIMULADOR INTERACTIVO PARA CALCULAR PAGOS EN CUOTAS
 
+let nombre;
 
+// Pedir al usuario que ingrese su nombre hasta que sea un texto válido
+while (true) {
+  nombre = prompt("Ingrese su nombre por favor");
+  if (isNaN(nombre)) {
+    break; // El nombre es válido, finaliza el bucle
+  } else {
+    alert("Por favor, ingrese un nombre válido.");
+  }
+}
 
-// SIMULADOR INTERACTIVO PARA CALCULAR PAGOS EN CUOTAS 
+console.log(nombre);
 
-//nombre del cliente, el precio del producto y cantidad de cuotas 
+let precioProducto = parseInt(prompt(`${nombre} ingrese el precio del producto`));
+console.log(precioProducto);
 
-let nombre = prompt("Ingrese su nombre por favor")
-console.log(nombre)
-let precioProducto = parseInt(prompt(`${nombre} ingrese el precio del producto`))
-console.log(precioProducto)
-let cuotas = parseInt(prompt(`puede abonar el producto en 3, 6 o 9 cuotas exclusivamente, ingrese la cantidad que desea solicitar`))
-console.log(cuotas)
+let cuotas;
+
+// Pedir al usuario que ingrese la cantidad de cuotas hasta que sea 3, 6 o 9
+while (true) {
+  cuotas = parseInt(prompt(`Puede abonar el producto en 3, 6 o 9 cuotas exclusivamente. Ingrese la cantidad que desea solicitar:`));
+  if (cuotas === 3 || cuotas === 6 || cuotas === 9) {
+    break; // La cantidad de cuotas es válida, finaliza el bucle
+  } else {
+    alert("Por favor, ingrese una cantidad válida de cuotas (3, 6 o 9).");
+  }
+}
+
+console.log(cuotas);
+
 const interes = 10;
 
 
@@ -22,6 +42,7 @@ function sumarIntereses(precio, cuotas, interes) {
     const precioTotal = precio + (precio * interesPorcentaje) / 100;
     const cuotaMensual = precioTotal / cuotas;
     const interesPorCuota = (precio * interesPorcentaje) / (100 * cuotas);
+
   
     console.log(`${nombre} El precio total a pagar en ${cuotas} cuotas con ${interes}% de interés c/u es: $${precioTotal.toFixed(2)}`);
     console.log(`El monto de cada cuota mensual será de: $${cuotaMensual.toFixed(2)}`);
